83747a9c97725c39ff8f7d81048a4a813ce64445
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-06 00:38:09 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,56 @@
+const buttonConverte01 = document.querySelector(".buttonConverte")
+const moedaSelect = document.querySelector(".moedas-a-escolher")
+
+function converteValores(){
+    const inputValor = document.querySelector(".input-valor").value
+    const valorMoeda01 = document.querySelector(".valor-moeda-01") // Valor em real
+    const valorMoeda02 = document.querySelector(".valor-moeda-02") // Outras moedas
+
+    const dolarDoDia = 5.1 // Dolar do dia
+    const euroDoDia = 6.2 // Euro do dia
+
+
+    if (moedaSelect.value == "dolar"){
+        // Se o select estiver selecionado o valor de 'dolar' entre aqui
+        valorMoeda02.innerHTML = new Intl.NumberFormat("en-us", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputValor / dolarDoDia) // O valor do input dividido pelo valor da moeda.
+
+    }
+
+    if (moedaSelect.value == "euro"){
+        // Se o select estiver selecionado o valor de 'euro' entre aqui
+        valorMoeda02.innerHTML = new Intl.NumberFormat("de-de", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValor / euroDoDia)
+    }
+
+
+    valorMoeda01.innerHTML = new Intl.NumberFormat("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputValor)
+    // Configuração padrão do JS para aparecer corretamente quando converter os valores. Real Brasileiro
+}
+
+
+function changeMoeda(){
+    const moedaNome = document.getElementById('moeda-nome') // não preciso colocar # na frente
+    const moedaImagem = document.getElementById('moeda-img')
+
+    if (moedaSelect.value == "dolar") {
+        moedaNome.innerHTML = "Dólar Americano"
+        moedaImagem.src = "./assets/dolar.png"
+    }
+
+    if (moedaSelect.value == "euro") {
+        moedaNome.innerHTML = "Euro" // trocando texto
+        moedaImagem.src = "./assets/euro.png" // trocando a imagem
+    }
+    converteValores() // Chamando a função quando o select trocar a imagem e converter junto para não confundir o usuário.
+}
+
+moedaSelect.addEventListener("change", changeMoeda)
+buttonConverte01.addEventListener("click", converteValores)
